bd4a0bb1340bcdc5ab5fd64c41d70634362be8aa
haoz33 <z513873698@gmail.com>
2022-07-02 02:03:41 +0100

    added state class configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,19 @@
 import getImport from "./get_import";
 import { parse } from "yaml";
-import { promises as fs } from "fs";
-import { FileSystemWatcher, window, workspace } from "vscode";
+import { FileSystemWatcher, workspace } from "vscode";
 import { getWorkspacePath } from "../util/get_workspace_file_path";
+import StateClassConfig from "./state_class_config";
 
 class Config {
   public packageImport: string | undefined;
+
+  public stateClassConfig: StateClassConfig;
   private watcher: FileSystemWatcher | undefined;
+
+  constructor() {
+    this.stateClassConfig = new StateClassConfig();
+  }
+
   async initialization() {
     this.packageImport = await this.getImportDirective();
     this.watcher = workspace.createFileSystemWatcher(
@@ -30,6 +37,7 @@ class Config {
 
   async dispose() {
     this.watcher?.dispose();
+    this.stateClassConfig.dispose();
   }
 }
 
---
+++ src/config/state_class_config.ts
@@ -0,0 +1,45 @@
+import { Disposable, window, workspace } from "vscode";
+
+export default class StateClassConfig {
+  public initialState!: string;
+  public defaultStates!: string[];
+  public isPrivate!: boolean;
+  private watcher!: Disposable;
+  constructor() {
+    this.setConfig();
+    this.addListener();
+  }
+
+  addListener() {
+    this.watcher = workspace.onDidChangeConfiguration((event) => {
+      const affected = event.affectsConfiguration("freezedBloc");
+      if (affected) {
+        this.setConfig();
+      }
+    });
+  }
+
+  setConfig() {
+    this.initialState = workspace
+      .getConfiguration("freezedBloc")
+      .get("initialState")!;
+    this.defaultStates = workspace
+      .getConfiguration("freezedBloc")
+      .get("defaultStates")!;
+
+    this.isPrivate = workspace
+      .getConfiguration("freezedBloc")
+      .get("privateDefaultStates")!;
+
+    if (this.defaultStates.includes(this.initialState) === false) {
+      window.showErrorMessage(
+        "Unable to match initial State to any state in Default States. Please check settings for any typo or errors."
+      );
+      return;
+    }
+  }
+
+  dispose() {
+    this.watcher.dispose();
+  }
+}
